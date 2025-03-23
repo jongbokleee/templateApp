3206661566f49dfdebfd790471bad592ca42363d
leejongbok <leejb024@gmail.com>
2025-03-23 10:18:28 +0900

    사용하지 않는 RetrofitClient부분 주석 (나중에 사용할 경우 있으면 복원)

--- app/src/main/java/com/bienbetter/application/api/HospitalApiService.kt
+++ app/src/main/java/com/bienbetter/application/api/HospitalApiService.kt
@@ -1,18 +1,13 @@
 package com.bienbetter.application.api
 
-import com.bienbetter.application.model.HospitalResponse
-import retrofit2.Call
-import retrofit2.http.GET
-import retrofit2.http.Query
-
 interface HospitalApiService {
-    @GET("getRegnHmcList")
-    fun getHospitals(
-        @Query("hmcNm") hmcNm: String?,
-        @Query("locAddr") locAddr: String?,
-        @Query("siDoCd") siDoCd: Int?,
-        @Query("siGunGuCd") siGunGuCd: Int?,
-        @Query("serviceKey", encoded = true) serviceKey: String = "",
-        @Query("_type") type: String = "json"
-    ): Call<HospitalResponse>
+//    @GET("getRegnHmcList")
+//    fun getHospitals(
+//        @Query("hmcNm") hmcNm: String?,
+//        @Query("locAddr") locAddr: String?,
+//        @Query("siDoCd") siDoCd: Int?,
+//        @Query("siGunGuCd") siGunGuCd: Int?,
+//        @Query("serviceKey", encoded = true) serviceKey: String = "",
+//        @Query("_type") type: String = "json"
+//    ): Call<HospitalResponse>
 }
--- app/src/main/java/com/bienbetter/application/api/RetrofitClient.kt
+++ app/src/main/java/com/bienbetter/application/api/RetrofitClient.kt
@@ -1,16 +1,12 @@
 package com.bienbetter.application.api
 
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
-
 object RetrofitClient {
-    private const val BASE_URL = "https://apis.data.go.kr/openapi/service/rest/HmcSearchService/"
-
-    val instance: HospitalApiService by lazy {
-        Retrofit.Builder()
-            .baseUrl(BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create()) // ✅ JSON 변환을 위한 GsonConverterFactory 추가
-            .build()
-            .create(HospitalApiService::class.java) // ✅ Retrofit 인터페이스를 생성하여 반환
-    }
+//    private const val BASE_URL = "https://apis.data.go.kr/openapi/service/rest/HmcSearchService/"
+//    val instance: HospitalApiService by lazy {
+//        Retrofit.Builder()
+//            .baseUrl(BASE_URL)
+//            .addConverterFactory(GsonConverterFactory.create()) // ✅ JSON 변환을 위한 GsonConverterFactory 추가
+//            .build()
+//            .create(HospitalApiService::class.java) // ✅ Retrofit 인터페이스를 생성하여 반환
+//    }
 }
